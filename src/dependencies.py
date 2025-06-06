ec767ef2ede5050b457f81ecb00ba813803f8fe3
Yurii Bandrivskyi <bandrivskiy@users.noreply.github.com>
2025-06-06 12:18:42 +0000

    update rgb-lib-v (#1)

--- src/dependencies.py
+++ src/dependencies.py
@@ -3,14 +3,16 @@ from typing import Tuple
 from rgb_lib import Wallet
 from src.wallet_utils import load_wallet_instance,create_wallet_instance  # adjust your actual import
 
-def get_wallet(xpub: str = Header(...)) -> Tuple[Wallet, object, str]:
-    wallet, online = load_wallet_instance(xpub)
+def get_wallet(xpub_van: str = Header(...),
+    xpub_col: str = Header(...)) -> Tuple[Wallet, object, str, str]:
+    wallet, online = load_wallet_instance(xpub_van, xpub_col)
     if not wallet or not online:
         raise HTTPException(status_code=400, detail="Wallet not initialized")
-    return wallet, online, xpub
+    return wallet, online, xpub_van, xpub_col
 
-def create_wallet(xpub: str = Header(...)) -> Tuple[Wallet, object, str]:
-    wallet, online = create_wallet_instance(xpub)
+def create_wallet( xpub_van: str = Header(...),
+    xpub_col: str = Header(...)) -> Tuple[Wallet, object, str,str]:
+    wallet, online = create_wallet_instance(xpub_van, xpub_col)
     if not wallet or not online:
         raise HTTPException(status_code=400, detail="Wallet not initialized")
-    return wallet, online, xpub
+    return wallet, online, xpub_van, xpub_col
--- src/routes.py
+++ src/routes.py
@@ -7,7 +7,7 @@ from rgb_lib import BitcoinNetwork, Wallet,AssetSchema
 from src.rgb_model import AssetNia, Backup, Balance, BtcBalance, DecodeRgbInvoiceRequestModel, DecodeRgbInvoiceResponseModel, FailTransferRequestModel, GetAssetResponseModel, IssueAssetNiaRequestModel, ListTransfersRequestModel, ReceiveData, Recipient, RefreshRequestModel, RegisterModel, RgbInvoiceRequestModel, SendAssetBeginModel, SendAssetBeginRequestModel, SendResult, Transfer, Unspent
 from fastapi import APIRouter, Depends
 import os
-from src.wallet_utils import BACKUP_PATH, create_wallet_instance, get_backup_path, remove_backup_if_exists, restore_wallet_instance, test_wallet_instance
+from src.wallet_utils import BACKUP_PATH, create_wallet_instance, get_backup_path, remove_backup_if_exists, restore_wallet_instance
 import shutil
 import uuid
 import rgb_lib
@@ -41,75 +41,79 @@ class CreateUtxosEnd(BaseModel):
 class AssetBalanceRequest(BaseModel):
     assetId: str
 
+@router.post("/wallet/generate_keys")
+def register_wallet():
+    send_keys = rgb_lib.generate_keys(NETWORK)
+    return send_keys
 
 @router.post("/wallet/register", response_model=RegisterModel)
-def register_wallet(wallet_dep: tuple[Wallet, object,str]=Depends(create_wallet)):
-    wallet, online ,xpub= wallet_dep
+def register_wallet(wallet_dep: tuple[Wallet, object,str,str]=Depends(create_wallet)):
+    wallet, online ,xpub_van, xpub_col= wallet_dep
     btc_balance = wallet.get_btc_balance(online, False)
     address = wallet.get_address()
     return { "address": address, "btc_balance": btc_balance }
 
 @router.post("/wallet/listunspents",response_model=List[Unspent])
-def list_unspents(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def list_unspents(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     unspents = wallet.list_unspents(online, False, False)
     return unspents
 
 @router.post("/wallet/createutxosbegin",response_model=str)
-def create_utxos_begin(req: CreateUtxosBegin, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def create_utxos_begin(req: CreateUtxosBegin, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     psbt = wallet.create_utxos_begin(online, req.upTo, req.num, req.size, req.feeRate, False)
     return psbt
 
 @router.post("/wallet/createutxosend",response_model=int)
-def create_utxos_end(req: CreateUtxosEnd, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def create_utxos_end(req: CreateUtxosEnd, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     result = wallet.create_utxos_end(online, req.signedPsbt, False)
     return result
 
 @router.post("/wallet/listassets",response_model=GetAssetResponseModel)
-def list_assets(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def list_assets(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     wallet.sync(online)
     assets = wallet.list_assets([AssetSchema.NIA])
     return assets
 
 @router.post("/wallet/btcbalance",response_model=BtcBalance)
-def get_btc_balance(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def get_btc_balance(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     btc_balance = wallet.get_btc_balance(online, False)
     return btc_balance
 
 @router.post("/wallet/address",response_model=str)
-def get_address(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet,online, xpub= wallet_dep
+def get_address(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet,online, xpub_van, xpub_col= wallet_dep
     address = wallet.get_address()
     return address
 
 @router.post("/wallet/issueassetnia",response_model=AssetNia)
-def issue_asset_nia(req: IssueAssetNiaRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,str = wallet_dep
+def issue_asset_nia(req: IssueAssetNiaRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     asset = wallet.issue_asset_nia(online, req.ticker, req.name, req.precision, req.amounts)
     return asset
 
 @router.post("/wallet/assetbalance",response_model=Balance)
-def get_asset_balance(req: AssetBalanceRequest, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, _,xpub = wallet_dep
+def get_asset_balance(req: AssetBalanceRequest, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, _,xpub_van, xpub_col = wallet_dep
     balance = wallet.get_asset_balance(req.assetId)
     return balance
 # ,response_model=DecodeRgbInvoiceResponseModel
 @router.post("/wallet/decodergbinvoice")
-def decode_rgb_invoice(req:DecodeRgbInvoiceRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet) ):
-    wallet, online,xpub = wallet_dep
+def decode_rgb_invoice(req:DecodeRgbInvoiceRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet) ):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     invoice_data = rgb_lib.Invoice(req.invoice).invoice_data()
     print("invoice data", invoice_data)
     return invoice_data
 
 @router.post("/wallet/sendbegin")
-def send_begin(req: SendAssetBeginRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def send_begin(req: SendAssetBeginRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     invoice_data = rgb_lib.Invoice(req.invoice).invoice_data()
-    print("request data",xpub, req.asset_id, req.amount)
+    print("request data",xpub_van, req.asset_id, req.amount)
     print("invoice data", invoice_data)
 
     resolved_amount = invoice_data.amount if invoice_data.amount is not None else req.amount
@@ -142,65 +146,65 @@ class SignPSBT(BaseModel):
     wallet_id: str
     xpub: str
 
-@router.post("/test/sign")
-def sign_psbt(req:SignPSBT):
-    wallet,online = test_wallet_instance(req.wallet_id,req.xpub, req.mnemonic)
-    print("signing psbt",req.psbt)
-    signed_psbt = wallet.sign_psbt(req.psbt)
+# @router.post("/test/sign")
+# def sign_psbt(req:SignPSBT):
+#     wallet,online = test_wallet_instance(req.wallet_id,req.xpub, req.mnemonic)
+#     print("signing psbt",req.psbt)
+#     signed_psbt = wallet.sign_psbt(req.psbt)
 
-    print("signed_psbt", signed_psbt)
-    return {"signed_psbt":signed_psbt}
+#     print("signed_psbt", signed_psbt)
+#     return {"signed_psbt":signed_psbt}
 
 @router.post("/wallet/sendend", response_model=SendResult)
-def send_begin(req: SendAssetEndRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def send_begin(req: SendAssetEndRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     result = wallet.send_end(online, req.signed_psbt, False)
     return result
 
 @router.post("/blindreceive", response_model=ReceiveData)
-def generate_invoice(req: RgbInvoiceRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def generate_invoice(req: RgbInvoiceRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     receive = wallet.blind_receive(req.asset_id, req.amount, 3600, [PROXY_URL], 1)
     return receive
 
 @router.post("/wallet/failtransfers")
-def failtransfers(req: FailTransferRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def failtransfers(req: FailTransferRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     print("Failing transfers",req.batch_transfer_idx)
     failed = wallet.fail_transfers(online, req.batch_transfer_idx, req.no_asset_only, req.skip_sync)
     print("Failing res",failed)
     return {'failed': failed}
 
 @router.post("/wallet/listtransactions")
-def list_transaction(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online ,xpub= wallet_dep
+def list_transaction(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online ,xpub_van, xpub_col= wallet_dep
     list_transactions = wallet.list_transactions(online, False)
     return list_transactions
 
 @router.post("/wallet/listtransfers",response_model=List[Transfer])
-def list_transfers(req:ListTransfersRequestModel, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def list_transfers(req:ListTransfersRequestModel, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     
     list_transfers = wallet.list_transfers(req.asset_id)
     return list_transfers
 
 @router.post("/wallet/refresh")
-def refresh_wallet(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def refresh_wallet(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     refreshed_transfers = wallet.refresh(online,None, [], False)
     return refreshed_transfers
 
 @router.post("/wallet/sync")
-def wallet_sync(wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online,xpub = wallet_dep
+def wallet_sync(wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online,xpub_van, xpub_col = wallet_dep
     wallet.sync(online)
     return {"message": "Wallet synced successfully"}
 
 @router.post("/wallet/backup")
-def create_backup(req:Backup, wallet_dep: tuple[Wallet, object,str]=Depends(get_wallet)):
-    wallet, online, xpub = wallet_dep
-    remove_backup_if_exists(xpub)
-    backup_path = get_backup_path(xpub)
+def create_backup(req:Backup, wallet_dep: tuple[Wallet, object,str,str]=Depends(get_wallet)):
+    wallet, online, xpub_van, xpub_col = wallet_dep
+    remove_backup_if_exists(xpub_van)
+    backup_path = get_backup_path(xpub_van)
     wallet.backup(backup_path, req.password)
 
     if not os.path.exists(backup_path):
@@ -208,7 +212,7 @@ def create_backup(req:Backup, wallet_dep: tuple[Wallet, object,str]=Depends(get_
 
     return {
         "message": "Backup created successfully",
-        "download_url": f"/wallet/backup/{xpub}"
+        "download_url": f"/wallet/backup/{xpub_van}"
     }
 @router.get("/wallet/backup/{backup_id}")
 def get_backup(backup_id):
@@ -225,15 +229,15 @@ def get_backup(backup_id):
 def restore_wallet(
     file: UploadFile = File(...),
     password: str = Form(...),
-    xpub: str = Form(...),
+    xpub_van: str = Form(...),
 ):
-    remove_backup_if_exists(xpub)
-    backup_path = get_backup_path(xpub)
+    remove_backup_if_exists(xpub_van)
+    backup_path = get_backup_path(xpub_van)
     with open(backup_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     try:
         # Restore wallet from backup
-        restore_wallet_instance(xpub, password, backup_path)
+        restore_wallet_instance(xpub_van, password, backup_path)
         return {"message": "Wallet restored successfully"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Failed to restore wallet: {str(e)}")
